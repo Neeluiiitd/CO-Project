1c9c0f03d6cdef7ab245b7ee02aea0e240f57059
riju-talk <talk.riju@gmail.com>
2023-06-07 14:36:45 +0530

    hello there

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
+def float_to_binary(number):
+    if number == 0:
+        return '0'
+
+    binary = ''
+    integer_part = int(float(number))
+    decimal_part = float(number) - integer_part
+
+    binary_integer = bin(integer_part)[2:]
+
+    binary_decimal = ''
+    while decimal_part > 0 and len(binary_decimal) < 5:
+        decimal_part *= 2
+        bit = int(decimal_part)
+        binary_decimal += str(bit)
+        decimal_part -= bit
+
+    exponent = len(binary_integer) - 1
+    biased_exponent = bin(exponent + 3)[2:].zfill(3)
+
+    mantissa = (binary_integer[1:] + binary_decimal)[:5]
+    mantissa = mantissa + (5 - len(mantissa)) * "0"
+
+    binary = biased_exponent + mantissa
+    return binary
+
+
 def binary_to_float(binary):
     if binary == '0':
         return 0.0
@@ -15,4 +42,4 @@ def binary_to_float(binary):
 
     return number
 
-print(binary_to_float("10001100"))
+print(binary_to_float(float_to_binary("3.25")))
